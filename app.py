ce8aef4cabf5260e135e31c2814272dcdc7fe15d
Dave Laffan <dave.laffan@outlook.com>
2019-06-19 12:56:07 +0000

    Link to mongoDB. Add base.html

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 import os
+import random
 from flask import Flask, redirect, render_template, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
-app.secret_key = 'h34dfuck'
+
+# secret.key tp be REMOVED prior to submission
+# Use an env variable instead
+app.secret_key = 'qjfg[73hzd<Gid#-h'
+
+app.config["MONGO_DBNAME"] = 'drinksdb'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    print("2")
     
-
+    # Get all kv pairs
+    allQuotes = (mongo.db.quotes.find_one ({},{ "_id": 0, "quote": 1}))
+    # Store just the value array as x
+    x = allQuotes.get("quote")
+    i = random.randrange(0, len(x),2)
+    quoteName = x[i]
+    quoteText = x[i+1]
+    print(quoteName, " - ", quoteText)
+    
+    
+    return render_template('index.html',
+    drinks = mongo.db.drinks.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
