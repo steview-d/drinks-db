51f8c9763b4d388efc055a77de4603e7bffe7233
Dave Laffan <dave.laffan@outlook.com>
2019-06-20 15:01:35 +0000

    Implement basic pagination

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import os
+import math, os
 import random
-from flask import Flask, redirect, render_template, request, session, url_for
-from flask_pymongo import PyMongo
+from flask import flash, Flask, redirect, render_template, request, session, url_for
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+
+#from flask_paginate import Pagination, get_page_parameter
 
 
 app = Flask(__name__)
@@ -16,8 +18,14 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+
+#test
+per_page = 9
+
 @app.route("/")
 def index():
+
+    ## Get Quotes - can be refactored - and should be!
     # Get all kv pairs
     allQuotes = (mongo.db.quotes.find_one ({},{ "_id": 0, "quote": 1}))
     # Store just the value array as x
@@ -25,12 +33,23 @@ def index():
     i = random.randrange(0, len(x),2)
     quoteName = x[i]
     quoteText = x[i+1]
-    print(quoteName, " - ", quoteText)
+    
+    
+    # Pagination
+    drinks_per_page = 9
+    current_page = int(request.args.get('current_page', 1))
+    total_drinks = mongo.db.drinks.count()
+    num_pages = range(1, int(math.ceil(total_drinks / drinks_per_page)) +1)
+    drinks = mongo.db.drinks.find().sort('_id', pymongo.ASCENDING).skip((current_page - 1) * drinks_per_page).limit(drinks_per_page)
     
     return render_template('index.html',
-    drinks = mongo.db.drinks.find(),
-    quoteName = quoteName,
-    quoteText = quoteText)
+        drinks = drinks,
+        current_page = current_page,
+        pages = num_pages,
+        categories = mongo.db.categories.find(),
+        quoteName = quoteName,
+        quoteText = quoteText)
+    
     
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -41,7 +60,9 @@ def login():
             if request.form['password'] == current_user['password']:
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
-            ## Flash Message Here
+            flash("Incorrect username and/or password. Please try again.")
+            return render_template('login.html')
+        flash("Username {} does not exist.".format(request.form['username']))
     return render_template('login.html')
     
     
@@ -57,14 +78,11 @@ def register():
         user_list = mongo.db.users
         check_existing = user_list.find_one({"userName": request.form['username']})
         if not check_existing:
-            #user_list.insert_one(request.form.to_dict())
             user_list.insert_one({"userName": request.form['username'], "password": request.form['password']});
             session['username'] = request.form['username']
             return redirect(url_for('index'))
-        
-        return redirect(url_for('login'))
-        ## Flash already exists message
-
+        flash("Sorry, username '{}' has been taken. Please choose another".format(request.form['username']))
+        return redirect(url_for('register'))
     return render_template('register.html')
 
     
