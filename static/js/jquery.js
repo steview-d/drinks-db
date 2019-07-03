e41c714f69794f856d7079ff4ebfb390ffcb8c43
Dave Laffan <dave.laffan@outlook.com>
2019-07-03 15:50:20 +0000

    Add option to remove ingredient fields from add drink screen

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -10,30 +10,39 @@ $(document).ready(function() {
         window.history.back();
     });
 
-    // Add Ingredients
+    // Add / Remove Ingredients
     var num_fields = 4;
+    var min_fields = 1;
     var max_fields = 10;
+    var num_list = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+    
+    // Add
     $('#add_ingredient').click(function() {
-        
         if (num_fields < max_fields) {
-        
-        var num_list = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
-        $('#ingredients-container').append(
-            "<div class='col s4'>"+
-                "<div class='input-field'>"+
-                    "<input id='ingredient_" + (num_fields +1) +"' name='ingredient_" + (num_fields +1) +"' type='text' class='validate'>"+
-                    "<label for='ingredient_" + (num_fields +1) +"'>Ingredient " + num_list[(num_fields)] +"</label>"+
+            $('#ingredients-container').append(
+                "<div class='col s4 ingredient_field'>"+
+                    "<div class='input-field'>"+
+                        "<input id='ingredient_" + (num_fields +1) +"' name='ingredient_" + (num_fields +1) +"' type='text' class='validate'>"+
+                        "<label for='ingredient_" + (num_fields +1) +"'>Ingredient " + num_list[(num_fields)] +"</label>"+
+                    "</div>"+
                 "</div>"+
-                "</div>"+
-                "<div class='col s2'>"+
+                "<div class='col s2 measure_field'>"+
                     "<div class='input-field'>"+
                         "<input id='measure_" + (num_fields +1) +"' name='measure_" + (num_fields +1) +"' type='text' class='validate'>"+
                         "<label for='measure_" + (num_fields +1) +"'>Measure " + num_list[(num_fields)] +"</label>"+
                     "</div>"+
-               "</div>"+
-            "</div>"
-        );
-        num_fields += 1;
+               "</div>"
+            );
+            num_fields += 1;
+        }
+    });
+    
+    // Remove
+    $('#remove_ingredient').click(function() {
+        if (num_fields > min_fields) {
+            $('.ingredient_field').filter(':last').remove()
+            $('.measure_field').filter(':last').remove()
+            num_fields -= 1;
         }
     });
 
