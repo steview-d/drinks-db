da8a7484ca53dde6081430ed5b08478058f29f63
Dave Laffan <dave.laffan@outlook.com>
2019-07-03 15:00:29 +0000

    Add option to add extra ingredients to add_drink screen
    Default number of ingredients and measures when adding a new drink is 4.
    Add button that uses js to add an extra ingredient and measure each
    click, making sure to also increment the id, name, and label number too.

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -11,8 +11,31 @@ $(document).ready(function() {
     });
 
     // Add Ingredients
+    var num_fields = 4;
+    var max_fields = 10;
     $('#add_ingredient').click(function() {
         
+        if (num_fields < max_fields) {
+        
+        var num_list = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+        $('#ingredients-container').append(
+            "<div class='col s4'>"+
+                "<div class='input-field'>"+
+                    "<input id='ingredient_" + (num_fields +1) +"' name='ingredient_" + (num_fields +1) +"' type='text' class='validate'>"+
+                    "<label for='ingredient_" + (num_fields +1) +"'>Ingredient " + num_list[(num_fields)] +"</label>"+
+                "</div>"+
+                "</div>"+
+                "<div class='col s2'>"+
+                    "<div class='input-field'>"+
+                        "<input id='measure_" + (num_fields +1) +"' name='measure_" + (num_fields +1) +"' type='text' class='validate'>"+
+                        "<label for='measure_" + (num_fields +1) +"'>Measure " + num_list[(num_fields)] +"</label>"+
+                    "</div>"+
+               "</div>"+
+            "</div>"
+        );
+        num_fields += 1;
+        }
     });
 
 });
+
