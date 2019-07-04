734096c51067be59f4d0991bb6dc1794957f95b1
Dave Laffan <dave.laffan@outlook.com>
2019-07-04 09:54:35 +0000

    Allow all form data to be added directly into drinks db

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -20,18 +20,19 @@ $(document).ready(function() {
     $('#add_ingredient').click(function() {
         if (num_fields < max_fields) {
             $('#ingredients-container').append(
-                "<div class='col s4 ingredient_field'>"+
-                    "<div class='input-field'>"+
-                        "<input id='ingredient_" + (num_fields +1) +"' name='ingredient_" + (num_fields +1) +"' type='text' class='validate'>"+
-                        "<label for='ingredient_" + (num_fields +1) +"'>Ingredient " + num_list[(num_fields)] +"</label>"+
-                    "</div>"+
-                "</div>"+
                 "<div class='col s2 measure_field'>"+
                     "<div class='input-field'>"+
                         "<input id='measure_" + (num_fields +1) +"' name='measure_" + (num_fields +1) +"' type='text' class='validate'>"+
                         "<label for='measure_" + (num_fields +1) +"'>Measure " + num_list[(num_fields)] +"</label>"+
                     "</div>"+
-               "</div>"
+                "</div>"+
+                "<div class='col s4 ingredient_field'>"+
+                    "<div class='input-field'>"+
+                        "<input id='ingredient_" + (num_fields +1) +"' name='ingredient_" + (num_fields +1) +"' type='text' class='validate'>"+
+                        "<label for='ingredient_" + (num_fields +1) +"'>Ingredient " + num_list[(num_fields)] +"</label>"+
+                    "</div>"+
+                "</div>"
+
             );
             num_fields += 1;
         }
@@ -40,8 +41,8 @@ $(document).ready(function() {
     // Remove
     $('#remove_ingredient').click(function() {
         if (num_fields > min_fields) {
-            $('.ingredient_field').filter(':last').remove()
             $('.measure_field').filter(':last').remove()
+            $('.ingredient_field').filter(':last').remove()
             num_fields -= 1;
         }
     });
